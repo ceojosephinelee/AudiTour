1d1318e3802ccec60cdff79c68a552920e0048b6
Jihyun <jose1123@naver.com>
2024-09-29 15:56:37 +0900

    첫 커밋

--- audi-tour/src/page/RegionListPage.js
+++ audi-tour/src/page/RegionListPage.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function RegionListPage() {
-  return (
-    <div>RegionListPage</div>
-  )
+  const [loading, setLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const getNames = async () => {
+    const json = await (
+      await fetch(
+        `https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=QtxR33BAWJG413oY8NxRSSLPG8FGqbBnNsQ%2FT%2BcUqxJPDeNS%2FV3PAvAp4a7n04VoR2hd8r2Q7ERrd9BrXn5S9w%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AudiTour&_type=json`
+      )
+    ).json();
+    setNames(json.response.body.item.name);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNames();
+  }, []);
+ 
 }
 
 export default RegionListPage
